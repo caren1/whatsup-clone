7f6565c385a360cfb9ff1b96f762914f0d98f1b7
caren1 <czarnocki.w@wp.pl>
2020-08-20 18:24:26 +0200

    initial commit - create-react-app + firebase config

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD5i_SVDWBd4cFsr46gt9kvSoa560_j_64",
+    authDomain: "whatsup-clone-wojt.firebaseapp.com",
+    databaseURL: "https://whatsup-clone-wojt.firebaseio.com",
+    projectId: "whatsup-clone-wojt",
+    storageBucket: "whatsup-clone-wojt.appspot.com",
+    messagingSenderId: "35496982346",
+    appId: "1:35496982346:web:75afdc61445f24f23eb3b3",
+    measurementId: "G-GN9BTH67GY"
+  };
