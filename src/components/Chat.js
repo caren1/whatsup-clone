7c4b1279f2904b2f5c6110f901d54540fe8aff1b
caren1 <czarnocki.w@wp.pl>
2020-08-20 20:29:05 +0200

    finished Chat.js component + styling

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -2,14 +2,22 @@ import React, { useState, useEffect } from 'react'
 import './Chat.css'
 import { Avatar, IconButton } from '@material-ui/core'
 import { AttachFile, MoreVert, SearchOutlined } from '@material-ui/icons'
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
+import MicIcon from '@material-ui/icons/Mic';
 
 function Chat() {
 
     const [ seed, setSeed ] = useState('')
+    const [ input, setInput ] = useState('')
 
     useEffect(() => {
         setSeed(Math.floor(Math.random() * 5000))
     }, [])
+
+    const sendMessage = (event) => {
+        event.preventDefault()
+        console.log(input)
+    }
 
     return (
         <div className="chat">
@@ -44,10 +52,18 @@ function Chat() {
                     Chat message
                 <span className="chat__timestamp">3:52pm</span>
                 </p>
-                
+
             </div>
             <div className="chat__footer">
-
+                    <InsertEmoticonIcon />
+                    <form>
+                        <input type="text"
+                         placeholder="Type a message"
+                         onChange={event => setInput(event.target.value)}
+                         value={input} />
+                        <button onClick={sendMessage} type="submit">Send a message</button>
+                    </form>
+                    <MicIcon />
             </div>
         </div>
     )
