8fc2ca01cadcc8b143ed59bbd47f110b1039bc14
caren1 <czarnocki.w@wp.pl>
2020-08-20 19:31:07 +0200

    generating an avatar, based on avatars.dicebar api

--- src/components/SidebarChat.js
+++ src/components/SidebarChat.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './SidebarChat.css'
+import { Avatar } from '@material-ui/core'
 
 function SidebarChat() {
+
+    const [ seed, setSeed ] = useState('')
+
+    useEffect(() => {
+        setSeed(Math.floor(Math.random() * 5000))
+    }, [])
+
+
     return (
         <div className="sidebarChat">
-            <h1>Hey</h1>
+            <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`}/>
+            <div className="sidebarChat__info">
+                <h2>Room name</h2>
+                <p>Last message...</p>
+            </div>
         </div>
     )
 }
